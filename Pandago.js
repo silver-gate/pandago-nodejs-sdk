31ec7eb32c95be9809956a2feadb4636722da0ba
John Huang <little78926@gmail.com>
2022-04-04 09:31:13 -0700

    bugfix for access token

--- Pandago.js
+++ Pandago.js
@@ -2,6 +2,8 @@ const axios = require('axios');
 const qs = require('qs');
 const nJwt = require('njwt');
 
+const MAX_RETRIES = 2;
+
 module.exports = class Pandago {
   constructor({
     env = 'sandbox',
@@ -11,21 +13,25 @@ module.exports = class Pandago {
     keyId,
     privateKey,
     scope = 'pandago.api.sg.*',
+    debug,
   }) {
     this.env = env;
-    this.authUrl = env === 'sandbox' ?
-      'https://sts-st.deliveryhero.io' :
-      'https://sts.deliveryhero.io';
-    this.apiUrl = env === 'sandbox' ?
-      `https://pandago-api-sandbox.deliveryhero.io/sg/api/${version}` :
-      `https://pandago-api-apse.deliveryhero.io/${countryCode}/api/${version}`;
+    this.authUrl = env === 'sandbox' ? 'https://sts-st.deliveryhero.io' : 'https://sts.deliveryhero.io';
+    this.apiUrl = env === 'sandbox' ? `https://pandago-api-sandbox.deliveryhero.io/sg/api/${version}` : `https://pandago-api-apse.deliveryhero.io/${countryCode}/api/${version}`;
     this.clientId = clientId;
     this.keyId = keyId;
     this.privateKey = privateKey;
     this.scope = scope;
+    this.debug = debug;
   }
 
-  _getSignedJwtToken() {
+  log(data) {
+    if (this.debug) {
+      console.log(data); // eslint-disable-line no-console
+    }
+  }
+
+  getSignedJwtToken() {
     const { clientId, keyId, privateKey } = this;
     const claims = {
       iss: clientId,
@@ -39,25 +45,32 @@ module.exports = class Pandago {
     return jwt.compact();
   }
 
-  async _getApiHeaders() {
+  async getApiHeaders(force = false) {
     const { accessTokenExpiredAt } = this;
 
-    if (!accessTokenExpiredAt || (accessTokenExpiredAt && accessTokenExpiredAt >= Date.now())) {
-      await this._getAccessToken();
+    if (force
+      || !accessTokenExpiredAt
+      || (accessTokenExpiredAt && accessTokenExpiredAt < Date.now())
+    ) {
+      await this.getAccessToken();
     }
 
     const { accessToken } = this;
 
     return {
       'Content-Type': 'application/json',
-      'Accept': 'application/json',
-      'Authorization': `Bearer ${accessToken}`,
+      Accept: 'application/json',
+      Authorization: `Bearer ${accessToken}`,
     };
   }
 
-  async _getAccessToken() {
-    const { clientId, authUrl, scope } = this;
-    const signedJwtToken = this._getSignedJwtToken();
+  async getAccessToken() {
+    const {
+      clientId,
+      authUrl,
+      scope,
+    } = this;
+    const signedJwtToken = this.getSignedJwtToken();
 
     const options = {
       method: 'POST',
@@ -74,9 +87,37 @@ module.exports = class Pandago {
       },
     };
 
-    const response = await axios(options);
-    this.accessToken = response.data.access_token;
-    this.accessTokenExpiredAt = Date.now() + response.data.expires_in * 1000;
+    const {
+      access_token: accessToken,
+      expires_in: accessTokenExpiredAt,
+    } = await this.request(options);
+
+    this.accessToken = accessToken;
+    this.accessTokenExpiredAt = Date.now() + accessTokenExpiredAt * 1000;
+  }
+
+  async request(payload, inRetries = 0) {
+    try {
+      this.log(payload);
+      const { data } = await axios(payload);
+      return data;
+    } catch (e) {
+      this.log(e);
+
+      if (e.response && e.response.status === 403 && inRetries < MAX_RETRIES) {
+        this.log('Retry for 403');
+        Object.assign(payload, {
+          headers: await this.getApiHeaders(true),
+        });
+        return this.request(payload, inRetries + 1);
+      }
+
+      if (e.response && e.response.data && e.response.data.message) {
+        throw new Error(e.response.data.message);
+      }
+
+      throw new Error(e.toJSON().message);
+    }
   }
 
   async estimateFee(order) {
@@ -85,11 +126,10 @@ module.exports = class Pandago {
       method: 'POST',
       url: `${apiUrl}/orders/fee`,
       data: order,
-      headers: await this._getApiHeaders(),
+      headers: await this.getApiHeaders(),
     };
 
-    const { data } = await axios(options);
-    return data;
+    return this.request(options);
   }
 
   async estimateTime(order) {
@@ -98,11 +138,10 @@ module.exports = class Pandago {
       method: 'POST',
       url: `${apiUrl}/orders/time`,
       data: order,
-      headers: await this._getApiHeaders(),
+      headers: await this.getApiHeaders(),
     };
 
-    const { data } = await axios(options);
-    return data;
+    return this.request(options);
   }
 
   async submitOrder(order) {
@@ -111,11 +150,10 @@ module.exports = class Pandago {
       method: 'POST',
       url: `${apiUrl}/orders`,
       data: order,
-      headers: await this._getApiHeaders(),
+      headers: await this.getApiHeaders(),
     };
 
-    const { data } = await axios(options);
-    return data;
+    return this.request(options);
   }
 
   async getOrder(orderId) {
@@ -123,11 +161,10 @@ module.exports = class Pandago {
     const options = {
       method: 'GET',
       url: `${apiUrl}/orders/${orderId}`,
-      headers: await this._getApiHeaders(),
+      headers: await this.getApiHeaders(),
     };
 
-    const { data } = await axios(options);
-    return data;
+    return this.request(options);
   }
 
   async cancelOrder(orderId, reason = 'REASON_UNKNOWN') {
@@ -141,11 +178,10 @@ module.exports = class Pandago {
       data: {
         reason,
       },
-      headers: await this._getApiHeaders(),
+      headers: await this.getApiHeaders(),
     };
 
-    const { data } = await axios(options);
-    return data;
+    return this.request(options);
   }
 
   async getOrderCurrentCourierLocation(orderId) {
@@ -153,11 +189,10 @@ module.exports = class Pandago {
     const options = {
       method: 'GET',
       url: `${apiUrl}/orders/${orderId}/coordinates`,
-      headers: await this._getApiHeaders(),
+      headers: await this.getApiHeaders(),
     };
 
-    const { data } = await axios(options);
-    return data;
+    return this.request(options);
   }
 
   async callback(event) {
@@ -166,11 +201,10 @@ module.exports = class Pandago {
       method: 'POST',
       url: `${apiUrl}/callback`,
       data: event,
-      headers: await this._getApiHeaders(),
+      headers: await this.getApiHeaders(),
     };
 
-    const { data } = await axios(options);
-    return data;
+    return this.request(options);
   }
 
   async createOrUpdateOutlet(id, outlet) {
@@ -179,11 +213,10 @@ module.exports = class Pandago {
       method: 'PUT',
       url: `${apiUrl}/outlets/${id}`,
       data: outlet,
-      headers: await this._getApiHeaders(),
+      headers: await this.getApiHeaders(),
     };
 
-    const { data } = await axios(options);
-    return data;
+    return this.request(options);
   }
 
   async getOutlet(id) {
@@ -191,10 +224,9 @@ module.exports = class Pandago {
     const options = {
       method: 'GET',
       url: `${apiUrl}/outlets/${id}`,
-      headers: await this._getApiHeaders(),
+      headers: await this.getApiHeaders(),
     };
 
-    const { data } = await axios(options);
-    return data;
+    return this.request(options);
   }
 };
